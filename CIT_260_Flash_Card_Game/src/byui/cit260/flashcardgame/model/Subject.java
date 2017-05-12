54cd4d2584daaa0041281c8a248652b912309c57
Rob <egbertrg@gmail.com>
2017-05-11 22:06:55 -0600

    11 May 2017 Rob finished Subject class

---
+++ CIT_260_Flash_Card_Game/src/byui/cit260/flashcardgame/model/Subject.java
@@ -0,0 +1,82 @@
+package byui.cit260.flashcardgame.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by EgbertRG on 5/11/2017.
+ */
+
+public class Subject implements Serializable {
+    private String subjectID;
+    private String subjectName;
+    private String subjectDescription;
+    private Question question;
+
+    public Subject() {
+    }
+
+    public String getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(String subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getSubjectDescription() {
+        return subjectDescription;
+    }
+
+    public void setSubjectDescription(String subjectDescription) {
+        this.subjectDescription = subjectDescription;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subject subject = (Subject) o;
+
+        if (subjectID != null ? !subjectID.equals(subject.subjectID) : subject.subjectID != null) return false;
+        if (subjectName != null ? !subjectName.equals(subject.subjectName) : subject.subjectName != null) return false;
+        if (subjectDescription != null ? !subjectDescription.equals(subject.subjectDescription) : subject.subjectDescription != null)
+            return false;
+        return question != null ? question.equals(subject.question) : subject.question == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = subjectID != null ? subjectID.hashCode() : 0;
+        result = 31 * result + (subjectName != null ? subjectName.hashCode() : 0);
+        result = 31 * result + (subjectDescription != null ? subjectDescription.hashCode() : 0);
+        result = 31 * result + (question != null ? question.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "subjectID='" + subjectID + '\'' +
+                ", subjectName='" + subjectName + '\'' +
+                ", subjectDescription='" + subjectDescription + '\'' +
+                ", question=" + question +
+                '}';
+    }
+}
