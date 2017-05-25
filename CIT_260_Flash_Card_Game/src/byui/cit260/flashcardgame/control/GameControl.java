50c7e2a50aa4bc1b5e6405f17c70bd3d7effde1f
Rob <egbertrg@gmail.com>
2017-05-25 08:47:43 -0600

    2017-05-25 Rob updated GameControl class.

--- CIT_260_Flash_Card_Game/src/byui/cit260/flashcardgame/control/GameControl.java
+++ CIT_260_Flash_Card_Game/src/byui/cit260/flashcardgame/control/GameControl.java
@@ -3,16 +3,17 @@ package byui.cit260.flashcardgame.control;
 /**
  * Created by EgbertRG on 5/23/2017.
  */
-import byui.cit260.flashcardgame.model.Game;
 import byui.cit260.flashcardgame.model.Player;
+import byui.cit260.flashcardgame.model.Game;
 import byui.cit260.flashcardgame.model.Subject;
+import byui.cit260.flashcardgame.model.PlayerQuestionHistory;
 
 public class GameControl {
-    public Game createGame(Player, Subject){return blah;}
+    public Game createGame(Player player, Subject subject){return Game;}
     public Game getGame(Game gameID){return Game;}
-    public Game listIncompleteGames(playerID){return Game[];}
-    public Game saveGame(gameID) {return;}
-    public Game deleteGame(GameID){;}
-    public Game updateScore(playerQuestionHistory) {return score;}
+    public Game listIncompleteGames(Player playerID){return Game[];}
+    public Game saveGame(Game gameID) {return Void;}
+    public Game deleteGame(Game gameID){;}
+    public Game updateScore(PlayerQuestionHistory playerQuestionHistory) {return score;}
 
 }
